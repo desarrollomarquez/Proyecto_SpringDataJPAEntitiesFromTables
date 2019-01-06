806883b37dad59683e0f18f850ed62669e757a8c
Diego Maŕquez <desarrollo.marquez@gmail.com>
2019-01-06 01:32:15 -0500

    Adicio Proyecto Spring Data JPA

---
+++ SpringDataJPAEntitiesTables/src/main/java/SpringDataJPAEntitiesTables/SpringDataJPAEntitiesTables/SpringDataJpaEntitiesTablesApplication.java
@@ -0,0 +1,14 @@
+package SpringDataJPAEntitiesTables.SpringDataJPAEntitiesTables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaEntitiesTablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaEntitiesTablesApplication.class, args);
+	}
+
+}
+
